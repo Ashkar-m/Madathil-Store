5526dc3fc9b3eb13b7877d3c21423f6d432e91c7
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2023-12-07 16:46:32 +0530

    output file

---
+++ index.js
@@ -0,0 +1,30 @@
+// $(document).ready(function(){
+//     $("#FormValidate").validate({
+//         rules:{
+//             fullName:{
+//                 required:true,
+//                 minlenght:3
+//             },
+//             place:{
+//                 required:true,
+//                 minlenght:3
+//             },
+//             moblie:{
+//                 required:true,
+//             }
+//         },
+//         message:{
+//             name:{
+//                 required:"Enter your name",
+//                 minlenght:"you have to enter atleast 3 characters"
+//             }
+
+//         }
+//     })
+// })
+$(document).ready(function() {
+    $('.show_hide').click(function(){
+        $(".spread").toggle();
+        $(this).val
+    });
+    });
